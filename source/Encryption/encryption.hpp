eeabc4db260482f63416bc754d940409a8ae9192
rachdy <98930889+Rachdy@users.noreply.github.com>
2024-12-08 17:29:09 +0100

    Update encryption.hpp
    i just have  created header file of encryption and added some definitions

--- source/Encryption/encryption.hpp
+++ source/Encryption/encryption.hpp
@@ -1,1 +1,18 @@
+/*
+=============================================================================
+encryption is responsible to encrypt file/folder and contains functions that 
+cann manage this task
+=============================================================================
+*/
+#ifndef ENCRYPTION_HPP
+#define ENCRYPTION_HPP
 
+namespace encryption {
+
+
+
+}
+
+// end of namespace encryption
+
+#endif
