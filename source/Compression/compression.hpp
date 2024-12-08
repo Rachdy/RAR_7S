5737f9eaad94d0fb36ee6645217348df33604e1a
rachdy <98930889+Rachdy@users.noreply.github.com>
2024-12-08 17:37:39 +0100

    Create compression.hpp
    i have created headre file of compression and added some definition

---
+++ source/Compression/compression.hpp
@@ -0,0 +1,24 @@
+
+/*
+=========================================================
+  Compression is responsible about compressing file/folder 
+  or any Media 
+==========================================================
+*/
+
+
+#ifndef COMPRESSION_HPP
+#define COMPRESSION_HPP
+
+
+//define of namespace compression
+
+namespace compression{
+    
+
+
+}
+
+//end of namespace compression
+
+#endif
