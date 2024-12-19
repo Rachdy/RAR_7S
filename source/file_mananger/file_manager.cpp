9f778c02545fb13d80f54f67cc2ca434551c1c72
rachdy <ismailrchdy@gmail.com>
2024-12-19 12:57:11 +0100

    i have added parameters for functions

--- source/file_mananger/file_manager.cpp
+++ source/file_mananger/file_manager.cpp
@@ -3,8 +3,25 @@
 #define FILE_MANANGER_CPP
 
 #include "file_manager.hpp"
+#include <fstream>
+#include <vector>
 
 namespace file_mananger{
+    std::fstream myobj;
+    std::vector<std::fstream> current_opened_files;
+    void open_file(std::string file_name){
+        myobj.open(file_name, std::ios::in);
+    }
+
+    void open_file_as_binary(std::string file_name){
+        myobj.open(file_name, std::ios::binary);
+    }
+
+    void close_file(std:: string file_name){
+
+    }
+
+
 
 
 
--- source/file_mananger/file_manager.hpp
+++ source/file_mananger/file_manager.hpp
@@ -8,28 +8,32 @@
 #ifndef FILE_MANANGER_HPP
 #define FILE_MANANGER_HPP
 
+#include <string>
+#include<fstream>
+#include <unordered_map>
+
     /*
-        TODO : add "return type and arguements" to all functions  
+        TODO : add "return type and arguements" to all functions
     */
 namespace file_mananger{
 
-    void open_file_as_text();
-    void open_file_as_binary();
-    void open_file();
-    void close_file();
-    void read_from_file();
-    void write_to_file();
+    void open_file_as_text(const std::string file_path);
+    void open_file_as_binary(const std::string file_path);
+    void open_file(const std::string file_path);
+    void close_file(const std:: string file_path);
+    void read_from_file(const std::string file_path);
+    void write_to_file(const std::string file_path);
 
-    void open_folder();
-    void close_folder();
+    void open_folder(const std::string folder_path);
+    void close_folder(const std::string folder_path);
 
-    void get_file_info();
-    void get_folder_info();
+    void get_file_info(const std::string file_path);
+    void get_folder_info(const std::string file_path);
 
-    // maybe we need those 
+    // maybe we need those
     void delete_file();
     void delete_folder();
-    
+
 }
 // end of namespace file_mananger
 
